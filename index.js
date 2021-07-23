3537b94bb23519516b78dee762e327c741a33d5c
ALARMich <55944078+alarmichanin@users.noreply.github.com>
2021-07-23 23:09:27 +0300

    smth strange

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World!")
+document.addEventListener("DOMContentLoaded",()=>{
+    function clock(seconds){
+        let inter = setInterval(()=>{
+            let main = document.querySelector("#main")
+            let timer = main.querySelector("#timer")
+            timer.innerHTML=seconds
+            console.log(seconds)
+            if(seconds!==0)
+            seconds--
+            else{
+            alert("TIME IS UP")
+            clearInterval(inter)
+        }
+        },1000)
+    }
+    clock(30) 
+}
+)
